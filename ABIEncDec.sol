62c1c97de3d979d01512df924a64516934b39dd1
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-16 00:00:28 +0530

    ABI encoding

---
+++ ABIEncDec.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract AbiEncDec {
+    function encodeData(
+        uint256 fixedNumber,
+        string memory dynamicString,
+        uint256[2] memory fixedArray,
+        uint256[] memory dynamicArray
+    ) public pure returns (bytes memory) {
+        return abi.encode(fixedNumber, dynamicString, fixedArray, dynamicArray);
+    }
+}

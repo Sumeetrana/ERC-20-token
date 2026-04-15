b6fe43694857d567ff3abd9163a6904454ed820d
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-16 00:01:00 +0530

    ABI decoding

--- ABIEncDec.sol
+++ ABIEncDec.sol
@@ -11,4 +11,14 @@ contract AbiEncDec {
     ) public pure returns (bytes memory) {
         return abi.encode(fixedNumber, dynamicString, fixedArray, dynamicArray);
     }
+
+    funtion decodeData(bytes memory data) public pure returns (uint256, string memory, uint256[2] memory, uint256[] memory) {
+        (
+            uint256 fixedNumber,
+            string memory dynamicString,
+            uint256[2] memory fixedArray,
+            uint256[] memory dynamicArray
+        ) = abi.decode(data, (uint256, string, uint256[2], uint256[]));
+        return (fixedNumber, dynamicString, fixedArray, dynamicArray);
+    }
 }

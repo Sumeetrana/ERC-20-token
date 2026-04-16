2197a4e58fbe2a9452981a6294b4b7cd0ab629b2
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-16 23:41:38 +0530

    Caller contract added

--- DelegateContract.sol
+++ DelegateContract.sol
@@ -9,3 +9,16 @@ contract CalledContract {
         someNumber = _num;
     }
 }
+
+contract Caller {
+    uint public someNumber; // Note that this is same variable name as CalledContract
+
+    function setNumber(address _calledContractAddress, uint _num) public {
+        // Prepare the data to call setNumber(uint) on the Called contract
+        bytes memory data = abi.encodeWithSignature("setNumber(uint256)", _num);
+
+        // Perform the delegateCall
+        (bool success, ) = _calledContractAddress.delegatecall(data);
+        require(success, "Delegatecall failed");
+    }
+}
